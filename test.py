8f2bc0b91870b7b5547856b81198508361aa5ac7
alinac1888 <alinac44@nycstudents.net>
2025-09-11 09:07:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello")
+import turtle
+from turtle import*
+t=turtle()
+t.shape(turtle)
+sidelength=200
+rotate=90
+def square(x,y):
+    for i in range(4):
+         t.forward(x)
+         t.left(y)
+t.done
