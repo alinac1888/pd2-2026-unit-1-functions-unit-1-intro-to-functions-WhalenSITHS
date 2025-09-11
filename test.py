f38066298e5b1b9cf7ef53ead8a2080fed401e94
alinac1888 <alinac44@nycstudents.net>
2025-09-11 09:20:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import turtle
-from turtle import*
-t=turtle()
+from turtle import *
+t = Turtle()
 t.shape(turtle)
 sidelength=200
-rotate=90
 def square(x,y):
     for i in range(4):
          t.forward(x)
          t.left(y)
-t.done
+t.done()
