72066d26cdf3bf68387debbf42fa5248194f908b
alinac1888 <alinac44@nycstudents.net>
2025-09-12 09:20:29 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import turtle
 from turtle import *
 t = Turtle()
-t.shape(turtle)
-sidelength=200
+t.shape(Turtle)
+sidelength=100
+rotate=90
 def square(x,y):
     for i in range(4):
-         t.forward(x)
-         t.left(y)
-t.done()
+    t.forward(x)
+    t.left(y)
+
+    t.done()
+""" name = input("What is your name")
+ print(name)
+ def add(x,y)
+   return x + y """
