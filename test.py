298f471c03c3d06dc01681ed7656318186cb7219
alinac1888 <alinac44@nycstudents.net>
2025-09-15 09:26:16 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,61 @@
 import turtle
 from turtle import *
 t = Turtle()
-for i in range(4):
-    t.forward(100)
-    t.left(90)
+def doublSquares(4):
+  length = 25
+  for i in range(4):
+  suare(length, 90)
+    length = length*2
+    doublSquares(5)
+  turtle.done()
 
-turtle.done()
 """ name = input("What is your name")
 print(name)
 def add(x,y):
    return x + y """
+#float
+#bill=decimal
+
+# students = [names,]
+#print(stuents[number])
+#for students in student:
+#  print(student)
+# adding a student
+# student.append("name")
+
+# true or false
+# x = true
+# y = False
+# evaluations use double ==
+#if y == False
+#  print("messsage")
+# else:
+#  print("essage") 
+
+# evaluations in list
+students = [list,]
+if "student" in students:
+   print("message")
+else: 
+   students.append
+   print("message")
+
+x = 10
+if x < 10:
+  print("less")
+elif x==10:
+  print("equal")
+else:
+  print("greater than 10")
+
+students = [students,]
+for student in students:
+    found = False
+    if student == "1":
+      print("Found 1")
+      found = True
+
+      name = "list"
+      # prints(name[0])
+      for letter in name:
+        print(letter)
